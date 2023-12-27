084f71555a7a04646ee8e8dbccdc520fa3510ad4
yaphoongchun <154993228+yaphoongchun@users.noreply.github.com>
2023-12-27 22:07:36 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c4omwmdsjzn9.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "yaphoongchun-employee"
+customregion = "us-east-1"
+
